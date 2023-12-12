9b93991f208a44f360e0980519e6df60f8fa75b1
Alex <90724908+lofty1337@users.noreply.github.com>
2023-12-12 11:13:59 +0200

    Create Rook.java

---
+++ lab4/src/Figures/Rook.java
@@ -0,0 +1,21 @@
+package Figures;
+
+public class Rook extends Figure{
+    public Rook(String name, char color) {
+        super(name, color);
+    }
+
+    @Override
+    public boolean canMove(int row, int col, int row1, int col1) {
+        if (row == row1 || col == col1){
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean canAttack(int row, int col, int row1, int col1) {
+        return this.canMove(row, col, row1, col1);
+    }
+}
